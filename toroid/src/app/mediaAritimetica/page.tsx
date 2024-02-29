e7af83af134ad53561175367c4c68743be021cb0
AlexandrePadilhaa <105119507+AlexandrePadilhaa@users.noreply.github.com>
2024-02-28 21:37:40 -0300

    fix deploy error

--- toroid/src/app/mediaAritimetica/page.tsx
+++ toroid/src/app/mediaAritimetica/page.tsx
@@ -4,7 +4,7 @@ import Formulario from "@/components/media/formulario";
 
 export default function mediaAritimetica() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
       <Formulario/>
     </div>
   );
